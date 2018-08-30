7db1bd2f5b9fc37643403fe4fb73f10dcd3f7a60
Marcelo Neres <marcelo.neres1@gmail.com>
2018-08-30 23:13:45 +0100

    Code refactoring

--- src/components/textFileUtils.js
+++ src/components/textFileUtils.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 class TextFileUtils {
   readTextFile(filePath){
     return new Promise((resolve, reject) => {
-
       if(!filePath){
         const err = new Error('File path is not defined');
         return reject(err); 
@@ -16,17 +15,6 @@ class TextFileUtils {
         return resolve(data.toString());
       });
     });
-  }
-
-  splitStringIntoArrayOfJson(str){
-    const response = [];
-    const arr = str.split("\n");
-    const promises = arr.map(async row => {
-      const jsonRow = JSON.parse(row);
-      response.push(jsonRow);
-    });
-    await Promise.all(promises);
-    return resolve(response);
   }
 }
 
--- test/components/textFileReaderTest.js
+++ test/components/textFileReaderTest.js
@@ -1,43 +1,33 @@
 "use strict";
 
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
 const assert = chai.assert;
 const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
-const config = require('../../configurations/config');
+const config = require("../../configurations/config");
 
-const TextFileUtils = require('../../src/components/textFileUtils');
+const TextFileUtils = require("../../src/components/textFileUtils");
 
-describe('readTextFilebyUrl()', () => {
-  it('should throw error', () => {
+describe("readTextFilebyUrl()", () => {
+  it("should throw error", () => {
     const textFileUtils = new TextFileUtils();
-    return assert.isRejected(textFileUtils.readTextFile(), "File path is not defined");
+    return assert.isRejected(
+      textFileUtils.readTextFile(),
+      "File path is not defined"
+    );
   });
-});
 
-describe('readTextFilebyUrl()', () => {
-  it('should throw error due to invalid path', () => {
+  it("should throw error due to invalid path", () => {
     const textFileUtils = new TextFileUtils();
     return assert.isRejected(textFileUtils.readTextFile("/invalid/path"));
   });
-});
 
-describe('readTextFilebyUrl()', () => {
-  it('should read text file by url and result not empty', () => {
+  it("should read text file by url and result not empty", () => {
     const textFileUtils = new TextFileUtils();
     return expect(
       textFileUtils.readTextFile(config.customerFile)
-    ).to.eventually.not.equal('');
+    ).to.eventually.not.equal("");
   });
 });
-
-// describe('splitStringIntoArrayOfJson()', () => {
-//   it('should convert text into json array', () => {
-//     const textFileUtils = new TextFileUtils();
-//     textFileUtils.readTextFile(config.customerFile).then(
-
-//     );
-//   });
-// });
