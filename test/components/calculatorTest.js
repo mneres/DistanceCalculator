e78c7128ce62ea7976fd3b363f757fb58a11386e
Marcelo Neres <neresm@innopharmalabs.com>
2018-08-31 17:02:48 +0100

    Refactor distance calculator test

--- test/components/calculatorTest.js
+++ test/components/calculatorTest.js
@@ -6,28 +6,26 @@ const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
 const calculator = require("../../src/components/calculator");
+const config = require('../../configurations/config');
 
-const referencePoint = {
-  latitude: 53.339428,
-  longitude: -6.257664
+const referencePoint = config.referencePoint;
+const customer = {
+  latitude: "53.1229599",
+  longitude: "-6.2705202",
+  result: 24.07,
+  wrongResult: 10
 };
-const customer = [
-  {
-    latitude: "53.1229599",
-    longitude: "-6.2705202"
-  }
-];
 
 describe("calculateDistance()", () => {
   it("should get the distance(KM) between 2 points", done => {
-    let distanceKm = calculator.calculateDistance(customer[0], referencePoint);
-    expect(distanceKm).to.be.equal(24.07);
+    let distanceKm = calculator.calculateDistance(customer, referencePoint);
+    expect(distanceKm).to.be.equal(customer.result);
     done();
   });
 
   it("should get the distance(KM) between 2 points with wrong result", done => {
-    let distanceKm = calculator.calculateDistance(customer[0], referencePoint);
-    expect(distanceKm).to.be.not.equal(10);
+    let distanceKm = calculator.calculateDistance(customer, referencePoint);
+    expect(distanceKm).to.be.not.equal(customer.wrongResult);
     done();
   });
 });
