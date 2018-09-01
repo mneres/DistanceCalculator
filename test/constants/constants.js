7dec2fba39eb9431d9993c9e9dd51e1b2c4eb676
Marcelo Neres <marcelo.neres1@gmail.com>
2018-09-01 16:04:07 +0100

    added data for test purposes

---
+++ test/constants/constants.js
@@ -0,0 +1,17 @@
+const CUSTOMER_STR =
+  '{"latitude": "51.8856167", "user_id": 2, \
+  "name": "Ian McArdle", "longitude": "-10.4240951"}';
+
+const CUSTOMERS = [
+  {
+    latitude: "53.1229599",
+    longitude: "-6.2705202",
+    result: 24.07,
+    wrongResult: 10
+  }
+];
+
+module.exports = {
+  CUSTOMER_STR,
+  CUSTOMERS
+};
